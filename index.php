10655099edb0bed408ab039c8d23e32ffa97271c
arsalansta <99049069+arsalansta@users.noreply.github.com>
2022-09-07 14:14:46 -0500

    Update and rename index.html to index.php

---
+++ index.php
@@ -0,0 +1,25 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Bootstrap demo</title>
+  </head>
+  <body>
+    <html>
+<body>
+  <h1>post response</h1>
+  
+Welcome <?php echo $_POST["name"]; ?><br>
+Your email address is: <?php echo $_POST["email"]; ?>
+
+</body>
+</html>
+    
+    
+    
+    
+    
+    
+  </body>
+</html>
