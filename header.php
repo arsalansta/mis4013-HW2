12fb3120353071a42e4058a46d62597eb8098180
arsalansta <99049069+arsalansta@users.noreply.github.com>
2022-09-19 13:22:41 -0500

    Update header.php

--- header.php
+++ header.php
@@ -1,11 +1,13 @@
-
-<? require_once("header.php"); ?>
-<h1> My website </h1>
-
-<form action="handlepost.php" method="post">
-Name: <input type="text" name="name"><br>
-E-mail: <input type="text" name="email"><br>
-<input type="submit">
-</form>    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-u1OknCvxWvY5kfmNBILK2hRnQC3Pr17a+RTT6rIHI7NnikvbZlHgTPOOmMi466C8" crossorigin="anonymous"></script>  </body>
-</html>
-
+<?php require_once("header.php"); ?> <!doctype html>
+<html lang = "en">
+<body>
+<h1 style="color:blue; text-align:center;">Welcome to Sana Yari's Homework 2!</h1> <p style="color:red; text-align:center;">Enter your Name and Email Address please!</p> <br></br>
+<p style="text-align:center;">
+<h2 style="text-align:center;"> POST - Function </h2> <form action="handlepost.php" method="post">
+<p style="text-align:center;"> Name: <input type="text" name="name"></p><br> <p style="text-align:center;"> E-mail: <input type="text" name="email"></p><br> <p style="text-align:center;"><input type="submit"></p>
+<br></br>
+<h2 style="text-align:center;"> GET - Function </h2>
+<form action="welcome_get.php" method="get">
+<p style="text-align:center;"> Name: <input type="text" name="name"></p><br> <p style="text-align:center;"> E-mail: <input type="text" name="email"></p><br>
+<p style="text-align:center;"><input type="submit"></p> <br></br>
+</form> </form> </html> </body>
