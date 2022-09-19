4666d3c9a5418560c2e291193ea6e976b28b35f4
arsalansta <99049069+arsalansta@users.noreply.github.com>
2022-09-19 13:22:31 -0500

    Update index.php

--- index.php
+++ index.php
@@ -1,20 +1,26 @@
-<? require_once("header.php");?>
-<h1> my website </h1>
-
-
-
-
-
-
-<form action="welcome.php" method="post">
-Name: <input type="text" name="name"><br>
-E-mail: <input type="text" name="email"><br>
-<input type="submit">
-</form>
-
-</body>
-</html>
-
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-u1OknCvxWvY5kfmNBILK2hRnQC3Pr17a+RTT6rIHI7NnikvbZlHgTPOOmMi466C8" crossorigin="anonymous"></script>
-  </body>
-</html>
+<!doctype html> <html lang="en">
+<head>
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css"
+rel="stylesheet" integrity="sha384-iYQeCzEYFbKjA/T2uDLTpkwGzCiq6soy8tYaI1GyVh/UjpbCx/TYkiZhlZB6 +fzT" crossorigin="anonymous">
+<title>Homework 2</title>
+</head>
+<body>
+<nav class="navbar navbar-expand-lg bg-light"> <div class="container-fluid">
+<a class="navbar-brand" href="#">HomeWork 2 - MIS 4013</a>
+<button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+<span class="navbar-toggler-icon"></span>
+ 
+</button>
+<div class="collapse navbar-collapse" id="navbarNav">
+<ul class="navbar-nav"> <li class="nav-item">
+<a class="nav-link active" aria-current="page" href="#">Home</a> </li>
+<li class="nav-item">
+<a class="nav-link" href="#">Features</a>
+</li>
+<li class="nav-item">
+<a class="nav-link" href="#">Pricing</a> </li>
+</ul> </div>
+</div> </nav>
+</body> </html>
