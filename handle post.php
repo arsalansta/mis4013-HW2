5a72beed35a4d453f79b27622b4a6e25893e82e9
arsalansta <99049069+arsalansta@users.noreply.github.com>
2022-09-24 17:47:56 -0500

    Update handle post.php

--- handle post.php
+++ handle post.php
@@ -1,4 +1,6 @@
-<html> <body>
+<html>
+<body>
 Welcome <?php echo $_POST["name"]; ?><br>
-your email address is: <?php echo $_POST["email"]; ?> </body>
+your email address is: <?php echo $_POST["email"]; ?>
+</body>
 </html>
